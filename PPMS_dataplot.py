cf555d69afdb0315c369c2a16b9a9f7cd02f6aa9
Jared Gdanski <35704788+JGdanski@users.noreply.github.com>
2024-05-14 16:39:08 -0400

    Update PPMS_dataplot.py
    Allow user to plot multiple curves on the same plot by passing a list of strings for both the x and y axis. Program will now make as many plots as there are unique entries in x_data.

--- PPMS_dataplot.py
+++ PPMS_dataplot.py
@@ -19,13 +19,13 @@ def parse_ppms_data(filename):
 
 def plot_ppms_data(filename, plot_title, x_data, y_data, x_label, y_label):
     """
-    Plot the PPMS data.
+    Plot the relevant PPMS data.
 
     Args:
     - filename (str): Path to the PPMS data file.
     - plot_title (str): Title of the plot.
     - x_data (str): Name of the column for the x-axis.
-    - y_data (str): Name of the column for the y-axis.
+    - y_data (arr): List of strings of the column for the y-axis.
     - x_label (str): Label for the x-axis.
     - y_label (str): Label for the y-axis.
     """
@@ -33,7 +33,8 @@ def plot_ppms_data(filename, plot_title, x_data, y_data, x_label, y_label):
     x_data = args.x_data
     y_data = args.y_data
     plt.figure()
-    plt.plot(data[x_data], data[y_data], marker='o', linestyle='-')
+    for i in len(y_data):
+        plt.plot(data[x_data], data[y_data[i]], marker='o', linestyle='-')
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.title(plot_title)
@@ -42,18 +43,16 @@ def plot_ppms_data(filename, plot_title, x_data, y_data, x_label, y_label):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Plot data from a PPMS data file.')
-    parser.add_argument('--filename' ,type=str, action='store', help='Path to the PPMS data file')
+    parser.add_argument('-f', '--filename' ,type=str, action='store', help='Path to the PPMS data file')
     parser.add_argument('-p', '--plot_title', type=str, default='PPMS Data Plot', help='Title of the plot')
-    parser.add_argument('--x_data', type=str, default='Temperature (K)', help='Name of the column for the x-axis')
-    parser.add_argument('--y_data', type=str, default='Bridge 1 Resistance (Ohms)', help='Name of the column for the y-axis')
-    parser.add_argument('--x_label', type=str, default='Temperature (K)', help='Label for the x-axis')
-    parser.add_argument('--y_label', type=str, default='Resistance (Ohms)', help='Label for the y-axis')
+    parser.add_argument('-x_data', type=str, default='Temperature (K)', help='Name of the column for the x-axis')
+    parser.add_argument('-ydata', nargs='+', default=[], help='List of strings for the ydata to plot from the dataframe')
+    parser.add_argument('-x_label', type=str, default='Temperature (K)', help='Label for the x-axis')
+    parser.add_argument('-y_label', type=str, default='Resistance (Ohms)', help='Label for the y-axis')
     args = parser.parse_args()
     
-    print(args.filename)
     file_path = args.filename
     print('Reading in PPMS data file '+ args.filename)
     data = parse_ppms_data(args.filename)
+    print('Plotting the following columns: '+ args.xdata)
     plot_ppms_data(args.filename, args.plot_title, args.x_data, args.y_data, args.x_label, args.y_label)
-
-
